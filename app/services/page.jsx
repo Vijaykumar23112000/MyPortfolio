06f3d4bee7a2ba19f1b00e1dd3f4a785130f5597
Vijaykumar23112000 <mathew5359302@gmail.com>
2024-06-25 14:57:28 +0530

    Day 5 : Services Impl

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,9 +1,40 @@
-const page = () => {
+"use client"
+
+import { BsArrowDownRight } from "react-icons/bs"
+import Link from "next/link"
+import { servicesData } from "@/components/data/Data"
+import { motion } from "framer-motion"
+
+const Services = () => {
     return (
-        <div>
-            Services page
-        </div>
+        <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+            <div className="container mx-auto">
+                <motion.div
+                    className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
+                >
+                    {
+                        servicesData.map((item, i) =>
+                            <div key={i} className="flex-1 flex flex-col justify-center gap-6 group">
+                                <div className="w-full flex justify-between items-center">
+                                    <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                                        {item.num}
+                                    </div>
+                                    <Link className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45" href={item.href}>
+                                        <BsArrowDownRight className="text-primary text-3xl" />
+                                    </Link>
+                                </div>
+                                <h2 className="font-bold text-[42px] leading-none text-white group-hover:text-accent transition-all duration-500">{item.title}</h2>
+                                <p className="text-white/60 ">{item.description}</p>
+                                <div className="border-b border-white/70 w-full"></div>
+                            </div>
+                        )
+                    }
+                </motion.div>
+            </div>
+        </section>
     )
 }
 
-export default page
+export default Services
